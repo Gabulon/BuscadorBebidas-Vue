6c86a1cefd4ce9206fed5eef53ad9fb74dd8dd2e
Gabulon <gabo.fajardo7@gmail.com>
2024-07-05 17:20:09 -0600

    Creando el formulario de busqueda

---
+++ src/components/Header.vue
@@ -0,0 +1,28 @@
+
+<script>
+    export default {
+        
+    }
+</script>
+<template>
+   <header>
+        <div class="mx-auto container px-5 py-16">
+            <div>
+
+            </div>
+            <form class="md:1/2 2x:w-1/3 bg-orange-400 my-32 p-10 rounded-lg shadow space-y-6">
+                <div class="space-y-4">
+                    <label class="block text-white uppercase font-extrabold text-lg" for="ingrediente">Nombre o Ingredientes</label>
+                    <input id="ingrediente" type="text" class="p-3 w-full rounded-lg focus:outline-none" placeholder="Nombre o ingrediente: ej Vodka, Teuila, etc">
+                </div>
+                <div class="space-y-4">
+                    <label class="block text-white uppercase font-extrabold text-lg" for="categoria">categoría</label>
+                    <select id="categoria"  class="p-3 w-full rounded-lg focus:outline-none" >
+                        <option value="Seleccione"></option>
+                    </select>
+                </div>
+                <input type="submit" class="bg-orange-800 hover:bg-orange-900 cursor-pointer text-white font-extrabold w-full p-2 rounded-lg uppercase" value="Buscar Recetas"/>
+            </form>
+        </div>
+   </header>
+</template>
