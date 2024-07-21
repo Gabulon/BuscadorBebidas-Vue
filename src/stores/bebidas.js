666500be0ec1efba76c3b739f6add5282c604607
Gabulon <gabo.fajardo7@gmail.com>
2024-07-21 13:16:26 -0600

    Comunicación entre stores para mostrar y ocultar el modal

--- src/stores/bebidas.js
+++ src/stores/bebidas.js
@@ -3,21 +3,27 @@ import { defineStore } from 'pinia'
 import APIService from '../services/APIService'
 
 export const useBebidasStore = defineStore('bebidas', () => {
+
   const busqueda = reactive({
     nombre: '',
     categoria: ''
+
 })
 
 const recetas =ref([])
 const receta = ref({})
 
 onMounted(async function() {
+
   const {data: {drinks}} = await APIService.obtenerCategorias()
   categorias.value = drinks
+
 })
 async function obtenerRecetas() {
+
   const {data: {drinks}} =  await APIService.buscarRecetas(busqueda)
   recetas.value = drinks
+  
 }
   return {
     categorias,
