4099f36529b3fd562ad93c2fa08c327511ce808d
Gabulon <gabo.fajardo7@gmail.com>
2024-07-21 13:11:04 -0600

    Creando un store para el modal

--- src/router/index.js
+++ src/router/index.js
@@ -2,8 +2,10 @@ import { createRouter, createWebHistory } from 'vue-router'
 import InicioView from '../views/InicioView.vue'
 
 const router = createRouter({
+
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
+
       {
         path: '/',
         name: 'inicio',
@@ -14,6 +16,7 @@ const router = createRouter({
         name: 'favoritos',
         component: () => import('../views/FavoritosView.vue')
       }
+      
   ]
 })
 
