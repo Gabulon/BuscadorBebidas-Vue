41085f205cebea4a7c63e75970aabb6ce5952637
Gabulon <gabo.fajardo7@gmail.com>
2024-08-04 18:42:00 -0600

    Mostrando texto cuando no hay favoritos

--- src/services/APIService.js
+++ src/services/APIService.js
@@ -3,11 +3,12 @@ import api from "../lib/axios";
 export default {
 
     obtenerCategorias() {
-        
+
         return api('/list.php?c=list')
     },
     
     buscarRecetas({categoria, nombre}) {
+        
         return api(`/filter.php?c=${categoria}&i=${nombre}`)
     }
     
